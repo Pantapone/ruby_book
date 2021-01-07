0bb7e88bf2b326252e23ebf136585c87b9f19e45
Victoria Schoeffel <vns28cam.ac.uk>
2021-01-07 08:38:20 +0100

    update exercises

--- the_basics/exercise2.rb
+++ the_basics/exercise2.rb
@@ -1,1 +1,11 @@
+th = 1234 / 1000
+hund = 1234 % 1000 / 100
+tens = 1234 % 1000 % 100 / 10
+ones = 1234 % 1000 % 100 % 10
 
+
+# to check whether the solution is correct
+puts th
+puts hund
+puts tens
+puts ones
